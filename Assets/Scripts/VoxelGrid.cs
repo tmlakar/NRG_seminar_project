4e1c510dec3f4665bbf3914b0c1e73e5c62d880a
Tjaša M. <51084872+tmlakar@users.noreply.github.com>
2024-05-08 20:35:42 +0200

    started working on the voxelization of the scene

--- Assets/Scripts/VoxelGrid.cs
+++ Assets/Scripts/VoxelGrid.cs
@@ -4,6 +4,45 @@ using UnityEngine;
 
 public class VoxelGrid : MonoBehaviour
 {
+    public Vector3 boundsExtent = new Vector3(10, 10, 10);
+
+    public float voxelSize = 0.5f;
+
+    public int voxelsX, voxelsY, voxelsZ, numberOfVoxels;
+
+    public Vector3 GetVoxelResolution() {
+        return new Vector3(voxelsX, voxelsY, voxelsZ);
+    }
+
+    public Vector3 GetBoundsExtent() {
+        return boundsExtent;
+    }
+
+    public float GetVoxelSize() {
+        return voxelSize;
+    }
+
+   
+
+   // rendering the smoke
+
+
+   // easing function
+   float  Easing(float x) {
+        float ease = 0.0f;
+    
+        if (x < 0.5f) {
+            ease = 2*x*x;
+        } else {
+            ease = 1.0f - (1.0f / (5.0f * (2.0f * x - 0.8f) + 1));
+        }
+    
+        return Mathf.Min(1.0f, ease);
+   }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
